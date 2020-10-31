f71c8446c4e6896841627d66f4eaae875d9272bb
Mohammmed Sibahi <sibahi@live.com>
2020-11-01 01:27:13 +0200

    Creating the main design

---
+++ custom.js
@@ -0,0 +1,13 @@
+$(function() {
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: true,
+        autoPlay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        centerMode: true,
+        centerPadding: '0',
+      });
+});

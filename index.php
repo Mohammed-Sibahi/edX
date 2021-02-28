b4170d994e6e9ae6966c2efd2cf14d49192b48e5
Mohammed Sibahi <sibahi@live.com>
2021-02-28 04:11:09 +0200

    add header.php

--- index.php
+++ index.php
@@ -1,83 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+<?
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>edX Website</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+include_once 'header.php';
 
-  <link rel="stylesheet" type="text/css"
-    href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css" />
-  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
 
-  <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-  <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-      <a class="navbar-brand" href="#"><img src="logo1.png" class="navbar-logo"></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-              aria-haspopup="true" aria-expanded="false">
-              Courses
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Architecture</a>
-              <a class="dropdown-item" href="#">Art & Culture</a>
-              <a class="dropdown-item" href="#">Biology & Life Sciences</a>
-              <a class="dropdown-item" href="#">Business & Management</a>
-              <a class="dropdown-item" href="#">Chemistry</a>
-              <a class="dropdown-item" href="#">Communication</a>
-              <a class="dropdown-item" href="#">Computer Science</a>
-              <a class="dropdown-item" href="#">Data Analysis & Statistics</a>
-              <a class="dropdown-item" href="#">Electronics</a>
-              <a class="dropdown-item" href="#">Energy & Earth Sciences</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">All Subjects</a>
-            </div>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-              aria-haspopup="true" aria-expanded="false">
-              Programs & Degrees
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Schools & Partners</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">edX for Business</a>
-          </li>
-
-        </ul>
-      </div>
-      <form class="form-inline my-2 my-lg-0">
-        <i class="fa fa-search" aria-hidden="true"></i>
-        <button class="btn btn-outline-link my-2 my-sm-0" id="sign-in" type="submit">Sign in</button>
-        <button type="button" id="register" class="btn btn-outline-primary">Register</button>
-      </form>
-    </div>
-  </nav>
-
-  <main id="main-content" class="" name="main-content" tabindex="-1" aria-label="Content">
+?>
 
     <img style="width:100%"
       src="http://images.ctfassets.net/ii9ehdcj88bc/1rscYXV6y6gf3yOY9tGGyj/29c3946d30385e8b954e37a81181af68/979px.jpg">

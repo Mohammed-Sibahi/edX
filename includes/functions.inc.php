bc2470705768e2844bb82f3e5f887c2e2227b02c
Mohammed Sibahi <sibahi@live.com>
2021-03-02 08:34:26 +0200

    define the functions and make login functional

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -127,3 +127,56 @@ $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
     exit();
 
 }
+
+function emptyInputLogin( $username,  $pwd) {
+
+    $result;
+    
+    if (empty($username) || empty($pwd)) {
+
+        $result = true;
+
+    }
+    else {
+        $result = false;
+    }
+    return $result;
+
+}
+
+function loginUser($conn, $username, $pwd) {
+
+    $uidExists = uidExists($conn, $username, $username);
+
+    if ($uidExists === false) {
+
+        header("location: ../login.php?error=wronglogin");
+        exit();
+    }
+
+$pwdHashed = $uidExists["usersPwd"];
+
+$checkPwd = password_verify($pwd, $pwdHashed);
+
+
+if ($checkPwd === false) {
+    
+    header("location: ../login.php?error=wronglogin");
+    exit();
+
+}
+else if ($checkPwd === true) {
+
+    session_start();
+
+    $_SESSION["userid"] = $uidExists["usersId"];
+
+    $_SESSION["useruid"] = $uidExists["usersuid"];
+    
+    header("location: ../index.php");
+    exit();
+
+}
+
+
+}
